26c8bd5e5d545a45e6b7793074de4bcffbc79bfe
BONiii1212 <641564581@qq.com>
2022-04-17 21:13:16 +0800

    fix:

--- component/Footer.js
+++ component/Footer.js
@@ -2,7 +2,7 @@ export default function Footer(){
     return(
         <div className="footer-div">
             <div>@2022</div>
-            <div>BONiii个人博客</div>
+            <div>BONiii的个人博客</div>
         </div>
     )
 }
